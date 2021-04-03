462694e3adba272e43f94ff8e4c91d5f958b431b
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2021-04-03 14:09:22 +0200

    Add publish to maven central workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,24 @@
-
-plugins {
-    kotlin("jvm") version "1.4.21"
-}
-
 repositories {
     mavenCentral()
-    jcenter()
 }
+
+plugins {
+    `maven-publish`
+    kotlin("jvm")
+    id("org.ajoberstar.grgit") version "4.1.0"
+    id("com.utopia-rise.maven-central-publish")
+}
+
 dependencies {
-    implementation("junit:junit:4.12")
+    testImplementation("junit:junit:4.12")
+}
+
+publishing {
+    publications {
+        val godotLibraryPublication by creating(MavenPublication::class) {
+            artifactId = "jvm-godot-resource-serialization"
+            description = "Godot resource data model serialization api for JVM"
+            from(components.getByName("java"))
+        }
+    }
 }
